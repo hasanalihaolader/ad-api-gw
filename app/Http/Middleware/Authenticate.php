2d4fdc643d657f5b4c4d40e67527b335aaec7a97
bs23.hasan <hasan.haolader@brainstation-23.com>
2023-09-18 01:03:35 +0600

    feature: retrieve token and refresh token with user profile using JWT

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Http\Response;
 
 class Authenticate
 {
@@ -36,7 +37,13 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
+            return response()->json(
+                responseData(
+                    false,
+                    Response::HTTP_UNAUTHORIZED,
+                    'Unauthorized',
+                )
+            );
         }
 
         return $next($request);

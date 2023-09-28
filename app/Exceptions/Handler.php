b6e1e46474052b335796aa1aa1d5078e2fb0f3e3
bs23.hasan <hasan.haolader@brainstation-23.com>
2023-09-29 02:21:27 +0600

    feature: set error handle to send json response during api have any error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -40,15 +40,32 @@ class Handler extends ExceptionHandler
 
     /**
      * Render an exception into an HTTP response.
+     * Updated to return json for a request that wantsJson
+     * i.e: specifies
+     *      Accept: application/json
+     * in its header
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
-     *
-     * @throws \Throwable
+     * @param  \Exception  $e
+     * @return \Illuminate\Http\Response
      */
-    public function render($request, Throwable $exception)
+    public function render($request, Throwable $e)
     {
-        return parent::render($request, $exception);
+        return response()->json(
+            responseData(
+                false,
+                $this->getExceptionHTTPStatusCode($e),
+                $e->getMessage()
+            ),
+            $this->getExceptionHTTPStatusCode($e)
+        );
+    }
+
+    protected function getExceptionHTTPStatusCode($e)
+    {
+        // Not all Exceptions have a http status code
+        // We will give Error 500 if none found
+        return method_exists($e, 'getStatusCode') ?
+            $e->getStatusCode() : 500;
     }
 }

609a7676798eef46d5f255116bb51f0453555157
bs23.hasan <hasan.haolader@brainstation-23.com>
2023-09-29 19:04:12 +0600

    feature: finalize docker folder structure

--- config/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'service_id' => env('APP_SERVICE_ID', 'F67nzek6dbjcnBJqqTri1BWPxX36fpAj'),
-    'service_key' => env('APP_SERVICE_KEY', 'TMbF54aZx8bA9iGjQGQuXrFThu9ZVx4b'),
-    'service_name' => env('APP_SERVICE_NAME', 'apigw'),
-];
